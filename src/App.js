ea30952154cea094a153221a3e772c1f10c754cc
Martin Downes <mndowne@gmail.com>
2021-11-22 18:48:09 -0600

    Added a 404 not found page.

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import Navbar from './Navbar';
 import Home from './Home';
 import Create from './Create';
+import NotFound from './NotFound';
 import BlogDetails from './BlogDetails';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 
@@ -15,6 +16,7 @@ function App() {
                     <Route path="/" element={<Home />} />
                     <Route path="/create" element={<Create />} />
                     <Route path="/blogs/:id" element={<BlogDetails />} />
+                    <Route path="*" element={<NotFound />} />
                 </Routes>
             </div>
 
