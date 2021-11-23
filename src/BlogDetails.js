380c691903c05c3291c33e50a0f50a4320178237
Martin Downes <mndowne@gmail.com>
2021-11-22 18:36:12 -0600

    Added the ability to create and delete blogs.

--- src/BlogDetails.js
+++ src/BlogDetails.js
@@ -1,10 +1,21 @@
 import { useParams } from 'react-router-dom';
 import useFetch from './useFetch';
+import { useNavigate } from 'react-router-dom';
 
 const BlogDetails = () => {
 
     const { id } = useParams();
     const {data : blog, error, isPending } = useFetch('http://localhost:8000/blogs/' + id );
+    const navigate = useNavigate();
+
+    const handleClick = () => {
+        fetch('http://localhost:8000/blogs/' + blog.id , {
+            method: 'DELETE'
+        }).then(() => {
+            navigate('/');
+        });
+
+    }
 
     return (
         <div className="blog-details">
@@ -15,6 +26,7 @@ const BlogDetails = () => {
                     <h2>{blog.title}</h2>
                     <p> Written by { blog.author}</p>
                     <p> { blog.body}</p>
+                    <button onClick={handleClick}>delete</button>
                 </article>
             )}
         </div>
